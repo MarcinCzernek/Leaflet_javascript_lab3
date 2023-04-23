7849730318c3a72befa3e5e3d1f9bac9f81c0243
MarcinCzernek <mustang.mcz@gmail.com>
2023-04-23 07:59:59 +0200

    Migrate to Node server

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/zad3_openAQ/LabOpenAQ.html');
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'zad3_openAQ', 'LabOpenAQ.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
